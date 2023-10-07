f1452e0a332cc1ff6288d053a030bd05a0ff9602
LL-oyiso <theo.tose.tt@gmail.com>
2023-10-07 22:00:27 +0200

    changes to content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import {NextUIProvider} from "@nextui-org/react";
+
 
 import Navigation from './components/Navigation';
+import Maincontent from './components/Maincontent';
 // import Maincontent from './components/Maincontent';
 
 
 function App() {
   return (
    <>
-<NextUIProvider>
+
   <Navigation />
- 
-  </NextUIProvider>  
+ <Maincontent />
+
  
 
    </>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,68 +1,53 @@
-import React from 'react'
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, NavbarMenuToggle,NavbarMenu,NavbarMenuItem} from "@nextui-org/react";
-import Maincontent from './Maincontent';
+import React , {useState} from 'react'
+
+import {FaBars}  from 'react-icons/fa'
+
 // import {AcmeLogo} from "./AcmeLogo.jsx";
 
 function Navigation()  {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const menuItems = [
-    "Subscriptions",
-    "Plans",
-    "About us",
-    "Plan" 
+ 
+ 
    
-  ];
-  return (
-    <div>
-    <Navbar onMenuOpenChange={setIsMenuOpen}>
-      <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden space-x-80"
-        />
-        <NavbarBrand>
-         
-          <p className="font-bold text-inherit">ACME</p>
-        </NavbarBrand>
-      </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-     
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              color={
-                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
-              className="w-full"
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </Navbar>
-    <Maincontent />
+    const [nav, setNav] = useState(false)
+    const handleNav = () =>
+    {
+        setNav(!nav)
+    }
+  return (
+    <div className='w-full min-h-[50px] flex justify-between items-center absolute  text-black  bg-gray-400 '>
+         <h1 className='p-4 font-bold text-2xl'>Port</h1>
+      <ul className='hidden sm:flex px-4'>
+       
+        <li className='text-xl '>
+          <a href ="/">Home</a>
+        </li>
+        <li className='text-xl '>
+        <a href ="#about">About us</a>
+        </li>
+        <li className='text-xl '>
+        <a href ="/">Updates</a>
+        </li>
+      </ul>
+      {/* Hamburger icon anything above small it will be hidden */}
+      <div onClick ={handleNav} className='sm:hidden z-10'>
+        <FaBars size ={20} className ='mr-4 cursor-pointer'/>
+      </div>
+
+      {/* Mobile menu */}
+      <div onClick ={handleNav} className={nav ? 'overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex-col': 'absolute top-0 h-screen left-[-100%] ease-in duration-300'}>
+        <ul className='h-full w-full text-center pt-12'>
+        <li className='text-2xl py-8'>
+          <a href ="/">Home</a>
+        </li>
+        <li className='text-2xl py-8'>
+        <a href ="#about">About us</a>
+        </li>
+        <li className='text-2xl py-8'>
+        <a href ="#update">Updates</a>
+        </li>
+        </ul>
+      </div>
     </div>
   )
 }
