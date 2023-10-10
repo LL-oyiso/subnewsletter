a8cfcdd8ce593c572a45b9dead134a8e71272c5d
LL-oyiso <theo.tose.tt@gmail.com>
2023-10-10 15:59:17 +0200

    changes to here and design

--- src/components/Booter.jsx
+++ src/components/Booter.jsx
@@ -4,20 +4,24 @@ export const Booter = () => {
   return (
     <div id ="subscribe">
 
-     <div className='w-full h-[300px] text-center'>
-      <h1 className="text-4xl font-[Montserrat]   text-center mt-10">SUBSCRIBE/SUBSCRIBE/SUBSCRIBE/SUBSCRIBE</h1>
-      <h2 className='text-center mt-10'>Sign up for our newsletter</h2>
-     <div><input className='mt-5 border-4 border-black w-[300px] h-[50px]'></input></div> 
-      <button className ="bg-black text-white px-6 mt-10 text-xl"> SUBSCRIBE</button>
-    </div>
 
-    <div className='w-full bg-black h-[300px] text-white font-[Monterrat]'>
-    <a href ="#hero"> <h1 className='   text-bold  font-[KronaOne] px-10 py-10 text-2xl'>RUN START </h1></a>
+    <div className='w-full bg-black h-[450px] text-white justify-between font-[Monterrat] flex'>
+      <div>
+     <a href ="#hero"> <h1 className='text-bold  font-[KronaOne] px-10 py-10 text-2xl'>RUN START </h1></a>
      <ul className='ml-10'>
      <a href ="#creators"><li>The Creators</li></a> 
      <a href ="#specs"> <li>Specs</li></a>
      <a href ="#subscribe"><li>Subscribe</li></a>
      </ul>
+      </div>
+      
+      <div className='px-10 border-3 border-slate-200 text'>
+      <h1 className="text-2xl font-[Montserrat]   text-center mt-10">SUBSCRIBE/SUBSCRIBE/SUBSCRIBE/SUBSCRIBE</h1>
+      <h2 className='text-center mt-10'>Sign up for our newsletter</h2>
+     <input className='mt-5 border-4 border-white  w-[300px] h-[50px]'></input>
+      <button className ="bg-white text-black px-6  mt-10 text-xl"> SUBSCRIBE</button>
+      </div> 
+    
     </div>
     </div>
     
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,11 +9,9 @@ const Hero = () => {
       {/* <img src ="" alt =" " className='w-full h-full object-cover'/> */}
       <div className='max-w-[1140px] m-auto  '>
         <div className='absolute top-[40%] w-full md:[30%] max-w-[1100px] text-center h-full flex flex-col text-white p-4'>
-
-          <h1 className=' text-9xl font-bold text-[#131212] font-[KronaOne]  '>RUN START</h1>
-          <div className='top-[10%]'>
-          <h1 className='  text-9xl font-bold text-[#f5dd59] font-[KronaOne] '>RUN START</h1>
-          </div>
+        <h1 className=' text-9xl font-bold text-[#131212] font-[KronaOne]  '>RUN START</h1>
+          
+         
           
           <h2 className='font -bold text-4xl py-10 italic  text-black rotate-10'>Hello</h2>
           
@@ -23,6 +21,10 @@ const Hero = () => {
             e non eaque, magni explicabo sit ad?
           </p> */}
         </div>
+        <div className='top-[40%]'>
+
+<h1 className='  text-9xl font-bold text-[#f5dd59] font-[KronaOne] '>RUN START</h1>
+</div>
       </div>
     </div>
   )
